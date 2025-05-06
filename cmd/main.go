debddf26f6ffe149c749b0bdd4a6f0f850666086
israelviana <rael232is@gmail.com>
2025-05-06 18:21:21 -0300

    fix context
    Signed-off-by: israelviana <rael232is@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"context"
 	"github.com/Tito-Seu-assistente-virtual-de-cripto/prices/internal/jobs/token_price_job"
 )
 
 func main() {
 	job := token_price_job.NewTokensPriceJob()
-	job.Start(context.Background())
+	job.Start()
 }
